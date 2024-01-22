4ae60c505c9376bdb9c47fafc1858cfc85cac216
nikolaswise <nikolaswise@gmail.com>
2024-01-22 13:11:02 +0100

    call function from module and return value

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from add import add
 
 app = Flask(__name__)
 
@@ -9,9 +10,17 @@ def hello():
 @app.route('/form', methods=['POST', 'GET'])
 def form():
     form_input = None
+    a = 0
+    b = 0
+
     if request.method == 'POST':
         form_input = request.form['userInput']
-    return render_template('form.html', input=form_input)
+        a = int(request.form['a'])
+        b = int(request.form['b'])
+    math = add(a, b)
+    return render_template('form.html',
+      input=form_input,
+      math=math)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000)
